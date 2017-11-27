3a83d252d0dd5ed7ab5ce4552f2026e7a7760fb0
Yoon Kim <helolsjava@gmail.com>
2017-11-26 23:30:58 -0800

    Day 5
    - 21 Function

--- dev1/playground.py
+++ dev1/playground.py
@@ -181,4 +181,59 @@ elif nameLen < 10:
 else:
     print("Your name is very long")
 
+# Day 5
 
+
+def ex():
+    print("Hello world")
+
+
+ex()
+
+
+def single(a):
+    print(a)
+
+
+single(9)
+
+
+def mult(a, b, c):
+    d = a * b
+    print(d+c)
+
+
+mult(2, 3, 4)
+
+
+def giver(a, b):
+    c = a + b
+    return c
+
+
+def taker(d, e):
+    output = giver(d, e)
+    return output
+
+
+print(taker(1, 5))
+
+
+def first():
+    print("This is a function")
+
+
+def intFun(intVal):
+    return intVal * 2
+
+
+def takesTwo(int1, int2):
+    return int1 * int2
+
+def functionInside(a, b, c):
+    print(takesTwo(intFun(a), b) * c)
+
+
+first()
+
+functionInside(7, 4, 2)
